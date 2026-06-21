3b264c381a80a0e89f2a8ac61354df6292a0dd83
Nata <tsibinanatali@gmail.com>
2026-06-22 02:14:55 +0300

    домашка 18.06

---
+++ HOMEWORK/18.06.2026.py
@@ -0,0 +1,49 @@
+import random
+# написать игру "угадай число"
+# компьютер загадывает число от 1 до 99
+# игрок отгадывает.
+# Если отгадано за 5 попыток то очко игроку иначе очко пк
+# Когда игрок не угадывает число пк говорит, загаданное
+# число больше введенного или меньше
+# По аналогии с игрой "камень-ножницы-бумага"
+# предусмотреть подсчета очков, возможность повторной иггры и вывод общей статистики по играм
+print("угадай число,загаданное компьютером ")
+user = input("Ваше имя ")
+kol_user = 0
+kol_pc = 0
+games_counter =0
+
+while games_counter < 2:
+    kol = 0
+    numbers_pc = random.randint(1,99)
+
+    while kol < 5:
+        num_user = int(input("Введите число "))
+        kol+=1
+        if (num_user != numbers_pc and kol==5):
+
+            print("победа пк")
+            kol_pc += 1
+        elif num_user < numbers_pc:
+            print("загаданное число больше введенного ")
+        elif num_user > numbers_pc:
+            print("загаданное число меньше введенного ")
+        else:
+            print("Вы отгадали ")
+            kol_user+=1
+            break
+    games_counter+=1
+print("счет игроков: ")
+print(f"{user} = {kol_user} \n ПК = {kol_pc}")
+if kol_user > kol_pc:
+    print(f"победу одержал {user} ")
+elif kol_user < kol_pc:
+    print(f"победу одержал ПК")
+else:
+    print(" ничья ")
+    a = input("вы хотите еще сыграть(Y/X)")
+    if a != "Y":
+        break
+
+
+
