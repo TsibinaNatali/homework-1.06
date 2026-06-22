051134fcc2a04c69943a13abeb510359956d819f
Nata <tsibinanatali@gmail.com>
2026-06-22 22:01:51 +0300

    домашка 18.06

--- HOMEWORK/18.06.2026.py
+++ HOMEWORK/18.06.2026.py
@@ -9,40 +9,44 @@ import random
 # предусмотреть подсчета очков, возможность повторной иггры и вывод общей статистики по играм
 print("угадай число,загаданное компьютером ")
 user = input("Ваше имя ")
-kol_user = 0
-kol_pc = 0
-games_counter =0
+while True:
+    kol_user = 0
+    kol_pc = 0
+    games_counter =0
 
-while games_counter < 2:
-    kol = 0
-    numbers_pc = random.randint(1,99)
+    while games_counter < 3:
+        kol = 0
+        numbers_pc = random.randint(1,99)
 
-    while kol < 5:
-        num_user = int(input("Введите число "))
-        kol+=1
-        if (num_user != numbers_pc and kol==5):
+        while kol < 5:
+            num_user = int(input("Введите число "))
+            kol+=1
+            if (num_user != numbers_pc and kol==5):
 
-            print("победа пк")
-            kol_pc += 1
-        elif num_user < numbers_pc:
-            print("загаданное число больше введенного ")
-        elif num_user > numbers_pc:
-            print("загаданное число меньше введенного ")
-        else:
-            print("Вы отгадали ")
-            kol_user+=1
-            break
-    games_counter+=1
-print("счет игроков: ")
-print(f"{user} = {kol_user} \n ПК = {kol_pc}")
-if kol_user > kol_pc:
-    print(f"победу одержал {user} ")
-elif kol_user < kol_pc:
-    print(f"победу одержал ПК")
-else:
-    print(" ничья ")
+                print("победа пк")
+                kol_pc += 1
+            elif num_user < numbers_pc:
+                print("загаданное число больше введенного ")
+            elif num_user > numbers_pc:
+                print("загаданное число меньше введенного ")
+            else:
+                print("Вы отгадали ")
+                kol_user+=1
+                break
+        games_counter+=1
+
+    print("\n-----ИТОГИ-----: ")
+    print("счет игроков: ")
+    print(f"{user} = {kol_user} \n ПК = {kol_pc}\n")
+    if kol_user > kol_pc:
+         print(f"победу одержал {user} ")
+    elif kol_user < kol_pc:
+        print(f"победу одержал ПК")
+    else:
+         print(" ничья ")
     a = input("вы хотите еще сыграть(Y/X)")
-    if a != "Y":
+    if a != "Y" and a != "y":
+        print("----ПОКА---- ")
         break
 
 
